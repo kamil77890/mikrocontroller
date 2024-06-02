0d9504295651cec1c5728fe28de951bef0cc912f
bambus <132773043+bambus80@users.noreply.github.com>
2024-06-02 12:53:06 +0200

    fix spelling mistakes

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 export const Info = async () => {
-  const responce = await axios.get("http://127.0.0.1:5000/status");
-  return responce;
+  const response = await axios.get("http://127.0.0.1:5000/status");
+  return response;
 };
 
 export const Turn_off = async (id) => {
-  const responce = await axios.get(`http://127.0.0.1:5000/${id}/turn_off`);
+  const response = await axios.get(`http://127.0.0.1:5000/${id}/turn_off`);
 };
 
 export const Turn_on = async (id) => {
-  const responce = await axios.get(`http://127.0.0.1:5000/${id}/turn_on`);
+  const response = await axios.get(`http://127.0.0.1:5000/${id}/turn_on`);
 };
 
 export const Turn_off_all = async () => {
-  const responce = await axios.get(`http://127.0.0.1:5000/off`);
+  const response = await axios.get(`http://127.0.0.1:5000/off`);
 };
 
 export const Turn_on_all = async () => {
-  const responce = await axios.get(`http://127.0.0.1:5000/on`);
+  const response = await axios.get(`http://127.0.0.1:5000/on`);
 };
