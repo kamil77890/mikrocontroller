3d734ba11d044044b17add65ca75b68daf326262
bambus <132773043+bambus80@users.noreply.github.com>
2024-06-13 19:01:41 +0200

    fdvrd vcftrbg vcgftvc

--- frontend/src/commponents/DeviceControlPanel/index.jsx
+++ frontend/src/commponents/DeviceControlPanel/index.jsx
@@ -2,15 +2,28 @@ import "./DeviceControlPanel.scss";
 import { Turn_on, Turn_off } from "../../utils";
 
 const DeviceControlPanel = ({ device }) => {
+  const handleDeviceRename = () => {
+    const newName = prompt("Enter new device name");
+    if (newName) {
+      console.log(newName);
+    }
+  };
   return (
     <div className="control-panel" key={device.id}>
-      <h3>{device.name}</h3>
+      <h2>{device.name}</h2>
       <p>
         Status: {device.status ? `Connected (${device.ip})` : "Not connected"}
       </p>
       <p>Last Connection: {device.time ? device.time : "Unknown"}</p>
       <button onClick={() => Turn_off(device.id)}>Turn Off</button>
       <button onClick={() => Turn_on(device.id)}>Turn On</button>
+      <div className="configuration">
+        <h2>Device "{device.name}" Configuration</h2>
+        <button onClick={() => handleDeviceRename()}>Rename</button>
+        <button className="important" onClick={() => {}}>
+          Remove from list
+        </button>
+      </div>
     </div>
   );
 };
